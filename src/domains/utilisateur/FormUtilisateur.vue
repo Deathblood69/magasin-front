b0ae30e9d7b71593c7506159e8450ff61926f717
MaximusCrowe <deathblood69002@gmail.com>
2024-08-06 23:00:13 -0400

    fix(utilisateur): Modification form utilisateur

--- src/domains/utilisateur/FormUtilisateur.vue
+++ src/domains/utilisateur/FormUtilisateur.vue
@@ -2,8 +2,6 @@
   import type {Utilisateur} from './utilisateur'
   import {VALIDATIONS_RULES} from '~/constants/validationsRules.const'
   import type {RoleInterface} from '~/types/role'
-  import {PATHS_API} from '~/constants/pathsAPI.const'
-  import {METHODE_HTTP} from '~/constants/methodeHTTP.const'
 
   /** === PROPS === */
 
@@ -17,58 +15,14 @@
     required: true
   })
 
-  /** === REFS === */
-
-  const usernameBeforeUpdate = ref(model.username)
   const passwordVisible = ref(false)
   const passwordConfirmation = ref('')
-  const confirmIdentifiant = ref('id' in model)
-  // const usernameUseAlready = ref(false)
-  const usernameField = ref()
 
   /** === COMPUTED === */
-
-  const path = computed(() => {
-    return `${PATHS_API.utilisateur}/identifiant/${model.value.username}`
-  })
 
   const computedIconPassword = computed(() =>
     passwordVisible.value ? 'mdi-eye' : 'mdi-eye-off'
   )
-
-  //regles de validation de l'identifiant
-  const rulesConfirmIdentifiant = computed(() => {
-    return (
-      confirmIdentifiant.value ||
-      'Veuillez valider ou modifier la valeur du champ'
-    )
-  })
-
-  //regles de verification d'unicite de l'identifiant
-  const rulesUniqueIdentifiant = computed(() => {
-    return (
-      !usernameUseAlready.value ||
-      "L'identifiant est déjà utilisé. Veuillez en choisir un autre."
-    )
-  })
-
-  /** Requête qui retourne true ou false en si le nom d'utilisateur est déjà utilisé */
-  const {
-    data: usernameUseAlready,
-    pending,
-    execute: checkIdentifiant
-  } = useFetchService(path, {
-    method: METHODE_HTTP.GET,
-    immediate: false,
-    watch: false,
-    transform: (value: string) => value === 'true'
-  })
-
-  /** === METHODES === */
-
-  onMounted(() => {
-    pending.value = false
-  })
 
   const validatePasswordConfirmation = () => {
     if (
@@ -91,53 +45,12 @@
       model.value.firstName.slice(1).toLowerCase()
     model.value.lastName = model.value.lastName.toUpperCase()
   }
-
-  /**
-   * Génère un identifiant pour l'utilisateur s'il n'a ni nom d'utilisateur ni confirmation d'identifiant.
-   * L'identifiant est créé en combinant le prénom et le nom de l'utilisateur en minuscules séparés par un point.
-   */
-  function propositionIdentifiant() {
-    if (
-      model.value.firstName &&
-      model.value.lastName &&
-      !model.value.username &&
-      !confirmIdentifiant.value
-    ) {
-      model.value.username = `${model.value.firstName.toLowerCase()}.${model.value.lastName.toLowerCase()}`
-    }
-  }
-
-  /**
-   * Permet, lors d'un changement d'identifiant, de devoir revalider la modification.
-   * (Permet aussi de regler les couleurs des messages en dessus du textfield)
-   */
-  function handleChangeIdentifiant() {
-    confirmIdentifiant.value = false
-  }
-
-  /**
-   * Confirme l'identifiant et valide le champ de nom d'utilisateur.
-   */
-  async function handleConfirmIdentifiant() {
-    if (model.value.username !== usernameBeforeUpdate.value) {
-      await checkIdentifiant()
-    } else {
-      usernameUseAlready.value = false
-    }
-    confirmIdentifiant.value = true
-    await nextTick(() => {
-      usernameField.value.validate()
-      if (usernameUseAlready.value) {
-        confirmIdentifiant.value = false
-      }
-    })
-  }
 </script>
 
 <template>
   <VTextField
     id="lastName-field"
-    v-model="user.lastName"
+    v-model="model.lastName"
     :rules="[VALIDATIONS_RULES.required, VALIDATIONS_RULES.nameFormat]"
     label="Nom"
     test-id="lastName"
@@ -145,7 +58,7 @@
   />
   <VTextField
     id="firstName-field"
-    v-model="user.firstName"
+    v-model="model.firstName"
     :rules="[VALIDATIONS_RULES.required, VALIDATIONS_RULES.nameFormat]"
     label="Prénom"
     test-id="prenom"
@@ -154,41 +67,15 @@
   <VTextField
     id="username-field"
     ref="usernameField"
-    v-model.trim="user.username"
-    :messages="confirmIdentifiant ? 'Identifiant valide' : ''"
-    :rules="[
-      VALIDATIONS_RULES.annudefFormat,
-      rulesConfirmIdentifiant,
-      rulesUniqueIdentifiant
-    ]"
+    v-model.trim="model.username"
+    :rules="[VALIDATIONS_RULES.annudefFormat]"
     label="Identifiant"
     test-id="identifiant"
     @input="handleUserInput"
-    @update:focused="propositionIdentifiant"
-    @update:model-value="handleChangeIdentifiant"
-    :loading="pending"
-  >
-    <template v-slot:message="{message}">
-      <div :class="confirmIdentifiant ? 'hint-username' : ''">
-        {{ message }}
-      </div>
-    </template>
-    <template v-slot:append>
-      <VBtn
-        :disabled="!user.username"
-        class="iconValidateIdentifiant"
-        color="primary"
-        icon="mdi-check"
-        size="large"
-        v-bind="props"
-        @click="handleConfirmIdentifiant"
-      />
-      <AppTooltip text="Valider l'identifiant" />
-    </template>
-  </VTextField>
+  />
   <VTextField
     id="password-field"
-    v-model="user.password"
+    v-model="model.password"
     :rules="[VALIDATIONS_RULES.passwordValid]"
     :type="passwordVisible ? 'text' : 'password'"
     data-testid="pwd"
@@ -219,7 +106,7 @@
   <AppSelect
     id="typeUtilisateur"
     label="Type de utilisateur"
-    v-model="model.typeUtilisateur"
+    v-model="model.roles"
     :items="typeUtilisateurs"
     :rules="[VALIDATIONS_RULES.notEmpty]"
     density="default"
