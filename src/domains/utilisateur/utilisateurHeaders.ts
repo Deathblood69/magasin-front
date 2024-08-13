519b8870cad268b259927dd2d05c6139787f220c
MaximusCrowe <deathblood69002@gmail.com>
2024-08-13 02:41:36 -0400

    fix(headers): Utilisateurs

--- src/domains/utilisateur/utilisateurHeaders.ts
+++ src/domains/utilisateur/utilisateurHeaders.ts
@@ -1,6 +1,9 @@
 import type {AppDataTableHeaders} from '~/types/headers'
 
 export const utilisateurHeaders: AppDataTableHeaders = [
-  {title: 'Nom', key: 'nom', align: 'start'},
+  {title: 'Nom', key: 'lastName', align: 'start'},
+  {title: 'Prénom', key: 'firstName', align: 'start'},
+  {title: 'Identifiant', key: 'username', align: 'start'},
+  {title: 'Profil(s)', key: 'roles', align: 'start'},
   {title: 'Actions', key: 'actions', align: 'start', sortable: false}
 ]
