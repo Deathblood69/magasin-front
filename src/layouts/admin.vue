a7a3b107db5a33ba4af048670cac95f82f833a6c
MaximusCrowe <deathblood69002@gmail.com>
2024-09-01 09:22:08 -0400

    fix(layout): Correction layout admin

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -14,21 +14,17 @@
 
   /**  REFS  **/
   const {mobile} = useDisplay()
-  const drawer = ref(!mobile.value)
-  const expandBtnValue = ref(false)
+
+  const rail = ref(mobile.value)
 
   /**  COMPUTED  **/
 
-  const rail = computed(() => {
-    if (!mobile.value) {
-      return expandBtnValue.value
-    } else {
-      return true
-    }
-  })
-
   async function handleGoAccueil() {
     await router.push(PAGES.accueil)
+  }
+
+  function toggleRail() {
+    rail.value = !rail.value
   }
 </script>
 
@@ -36,30 +32,26 @@
   <VApp>
     <VLayout class="rounded rounded-md">
       <AppNavigationDrawer
-        v-model="drawer"
         :rail="rail"
         :items="LIENS_MENU"
       >
         <template #prepend>
-          <VContainer @click="expandBtnValue = !expandBtnValue">
+          <VContainer @click="toggleRail">
             <VImg
               :src="IMAGES.logo"
               max-height="150px"
             />
-            <div
-              v-if="!expandBtnValue"
-              align="center"
-            >
-              {{ expandBtnValue ? '' : app.TITRE }}
+            <div align="center">
+              {{ rail ? '' : app.TITRE }}
             </div>
           </VContainer>
         </template>
         <template #append>
           <VDivider />
           <VBtn
-            :text="expandBtnValue ? '' : 'Accueil'"
+            :text="rail ? '' : 'Accueil'"
             prepend-icon="mdi-home"
-            :icon="expandBtnValue || mobile ? 'mdi-home' : false"
+            :icon="rail || mobile ? 'mdi-home' : false"
             :rounded="0"
             :block="true"
             size="large"
